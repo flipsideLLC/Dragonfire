03427ea79ab5d4dcddfc606fa90a2df1556d10a6
Trevor Weihrauch <trevor.weihrauch@jbhunt.com>
2021-08-07 15:03:53 -0400

    activity indicator

--- Screens/ChatScreen.js
+++ Screens/ChatScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import Constants from 'expo-constants';
 import { GiftedChat } from "react-native-gifted-chat";
+import { MaterialIndicator } from 'react-native-indicators';
+import _ from 'lodash';
 import Fire from '../Fire';
 
 export default class ChatScreen extends React.Component {
@@ -34,10 +36,24 @@ export default class ChatScreen extends React.Component {
     render() {
         const chat = <GiftedChat messages={this.state.messages} onSend={Fire.send} user={this.user} />;
 
-        return (
-            <SafeAreaView style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
-                {chat}
-            </SafeAreaView>
-        );
+        if (_.isEmpty(this.state.messages)) {
+            return (
+                <SafeAreaView style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
+                    <MaterialIndicator
+                        animating
+                        size={100}
+                        color='black'
+                    />
+                </SafeAreaView>
+            );
+        } else {
+            return (
+                <SafeAreaView style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
+                    {chat}
+                </SafeAreaView>
+            );
+
+        }
+
     }
 }
